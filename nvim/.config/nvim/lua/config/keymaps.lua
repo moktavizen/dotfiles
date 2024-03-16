2487cf526743359d4715c2d80522653b760d92d6
moktavizen <moktavizzen@gmail.com>
2024-03-16 14:50:23 +0700

    change keymap desc

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,7 +4,7 @@
 
 local map = vim.keymap.set
 -- buffers
-map("n", "<leader>bh", "<cmd>lua require('bufferline').go_to(1, true)<cr>", { desc = "Switch to Visible Buffer 1" })
-map("n", "<leader>bj", "<cmd>lua require('bufferline').go_to(2, true)<cr>", { desc = "Switch to Visible Buffer 2" })
-map("n", "<leader>bk", "<cmd>lua require('bufferline').go_to(3, true)<cr>", { desc = "Switch to Visible Buffer 3" })
-map("n", "<leader>bl", "<cmd>lua require('bufferline').go_to(4, true)<cr>", { desc = "Switch to Visible Buffer 4" })
+map("n", "<leader>bh", "<cmd>lua require('bufferline').go_to(1, true)<cr>", { desc = "Switch to Buffer 1" })
+map("n", "<leader>bj", "<cmd>lua require('bufferline').go_to(2, true)<cr>", { desc = "Switch to Buffer 2" })
+map("n", "<leader>bk", "<cmd>lua require('bufferline').go_to(3, true)<cr>", { desc = "Switch to Buffer 3" })
+map("n", "<leader>bl", "<cmd>lua require('bufferline').go_to(4, true)<cr>", { desc = "Switch to Buffer 4" })
