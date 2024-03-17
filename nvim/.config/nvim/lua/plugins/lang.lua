b9792ddac0174f85c865dc12236147ae02ed5c43
moktavizen <moktavizzen@gmail.com>
2024-03-17 17:34:26 +0700

    add php support in nvim

--- nvim/.config/nvim/lua/plugins/lang.lua
+++ nvim/.config/nvim/lua/plugins/lang.lua
@@ -5,8 +5,9 @@ return {
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, {
-          "css",
           "hyprlang",
+          "css",
+          "php",
           -- "dart", -- tree-sitter-dart is slow and stuttery
         })
       end
@@ -15,6 +16,15 @@ return {
     vim.filetype.add({
       pattern = { [".*/hypr.*%.conf"] = "hyprlang" },
     }),
+  },
+
+  -- LSP, Linter, Formatter
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "phpactor" })
+    end,
   },
 
   -- Markdown
@@ -26,8 +36,8 @@ return {
   -- Dart & Flutter
   {
     "akinsho/flutter-tools.nvim",
-    -- lazy = true,
-    lazy = false,
+    -- lazy = false,
+    ft = "dart",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "stevearc/dressing.nvim", -- optional for vim.ui.select
