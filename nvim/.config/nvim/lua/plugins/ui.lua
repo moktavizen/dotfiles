c61f1cf60e415f2aa162e0ccda694496861b3fc2
moktavizen <moktavizzen@gmail.com>
2024-03-17 12:58:17 +0700

    rename and change pin icon

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,7 +1,9 @@
 return {
-  "nvimdev/dashboard-nvim",
-  opts = function(_, opts)
-    local logo = [[
+  -- dashboard
+  {
+    "nvimdev/dashboard-nvim",
+    opts = function(_, opts)
+      local logo = [[
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡑⠄⣀⠀⠀⠰⡀⢢⣄⠀⡜⠽⣠⡄⠀⠸⢤⢷⠀⢀⡆⡰⠃⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣐⣕⠬⢳⡢⠤⢧⡦⣉⠳⢇⠹⡈⠓⣜⡇⢬⠠⣀⡶⠽⠑⠂⠉⠁⠢⡀⠀⠀⠀
 ⠀⠀⠀⠀⡀⣄⣠⣲⣍⣉⣉⣁⠀⠻⣵⡚⢴⡅⠢⡀⢓⠬⠀⠀⠙⠉⠳⣼⠑⠁⠀⠀⠀⠀⠀⠀⠀⠀⠁⡀⠀
@@ -26,57 +28,72 @@ return {
 ⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣯⠀⢺⣄⠈⢚⠑⠤⠤⠤⠤⢴⣾⠇⢀⡀⠀⠀⣷⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ]]
 
-    logo = string.rep("\n", 1) .. logo .. "\n"
+      logo = string.rep("\n", 1) .. logo .. "\n"
 
-    opts.config.header = vim.split(logo, "\n")
-    opts.config.center = {
-      {
-        action = "Telescope find_files",
-        desc = " Find file",
-        icon = " ",
-        key = "f",
-      },
-      {
-        action = "ene | startinsert",
-        desc = " New file",
-        icon = " ",
-        key = "n",
-      },
-      {
-        action = "Telescope oldfiles",
-        desc = " Recent files",
-        icon = " ",
-        key = "r",
-      },
-      {
-        action = "Telescope live_grep",
-        desc = " Find text",
-        icon = " ",
-        key = "g",
-      },
-      {
-        action = [[lua require("lazyvim.util").telescope.config_files()()]],
-        desc = " Config",
-        icon = " ",
-        key = "c",
-      },
-      {
-        action = 'lua require("persistence").load()',
-        desc = " Restore Session",
-        icon = " ",
-        key = "s",
-      },
-      {
-        action = "LazyExtras",
-        desc = " Lazy Extras",
-        icon = " ",
-        key = "x",
-      },
-    }
+      opts.config.header = vim.split(logo, "\n")
+      opts.config.center = {
+        {
+          action = "Telescope find_files",
+          desc = " Find file",
+          icon = " ",
+          key = "f",
+        },
+        {
+          action = "ene | startinsert",
+          desc = " New file",
+          icon = " ",
+          key = "n",
+        },
+        {
+          action = "Telescope oldfiles",
+          desc = " Recent files",
+          icon = " ",
+          key = "r",
+        },
+        {
+          action = "Telescope live_grep",
+          desc = " Find text",
+          icon = " ",
+          key = "g",
+        },
+        {
+          action = [[lua require("lazyvim.util").telescope.config_files()()]],
+          desc = " Config",
+          icon = " ",
+          key = "c",
+        },
+        {
+          action = 'lua require("persistence").load()',
+          desc = " Restore Session",
+          icon = " ",
+          key = "s",
+        },
+        {
+          action = "LazyExtras",
+          desc = " Lazy Extras",
+          icon = " ",
+          key = "x",
+        },
+      }
 
-    for _, button in ipairs(opts.config.center) do
-      button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-      button.key_format = "  %s"
-    end
-  end,
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
+    end,
+  },
+
+  -- bufferline
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        groups = {
+          items = {
+            require("bufferline.groups").builtin.pinned:with({ icon = "" }),
+          },
+        },
+      },
+    },
+  },
 }
