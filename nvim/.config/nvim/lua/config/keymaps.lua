56094582849288e3146804214838c2e0b6e87b11
moktavizen <moktavizzen@gmail.com>
2024-03-28 09:04:19 +0700

    add flutter-tools keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,8 @@ map("n", "<leader>bh", "<cmd>lua require('bufferline').go_to(1, true)<cr>", { de
 map("n", "<leader>bj", "<cmd>lua require('bufferline').go_to(2, true)<cr>", { desc = "Switch to Buffer 2" })
 map("n", "<leader>bk", "<cmd>lua require('bufferline').go_to(3, true)<cr>", { desc = "Switch to Buffer 3" })
 map("n", "<leader>bl", "<cmd>lua require('bufferline').go_to(4, true)<cr>", { desc = "Switch to Buffer 4" })
+
+-- flutter-tools
+map("n", "<leader>cte", "<cmd>FlutterEmulators<cr>", { desc = "Shows a list of emulators" })
+map("n", "<leader>ctq", "<cmd>FlutterQuit<cr>", { desc = "Ends a running session" })
+map("n", "<leader>ctr", "<cmd>FlutterRun<cr>", { desc = "Run the current project" })
