4da2830750720cfbcd34ff7ef0556bf816b239ca
moktavizen <moktavizzen@gmail.com>
2024-04-08 13:18:18 +0700

    remove colorful bracket

--- nvim/.config/nvim/lua/plugins/util.lua
+++ nvim/.config/nvim/lua/plugins/util.lua
@@ -1,14 +1,9 @@
 return {
-  -- enable colorful bracket pairs (depends on installed treesitter)
-  {
-    "HiPhish/rainbow-delimiters.nvim",
-    -- config = true, -- for some reason this gives an error
-  },
-
   -- enable css color highlight
   {
     "brenoprata10/nvim-highlight-colors",
     -- lazy = true,
+    ft = "css",
     config = true,
   },
 
