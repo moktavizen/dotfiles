7499b748bf374454f5e99a763fd14a681f8fb707
moktavizen <moktavizzen@gmail.com>
2024-05-16 11:19:28 +0700

    change branch and path display

--- nvim/.config/nvim/lua/plugins/ui/file_browser.lua
+++ nvim/.config/nvim/lua/plugins/ui/file_browser.lua
@@ -1,7 +1,6 @@
 return { -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
-  branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -53,7 +52,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         path_display = {
-          'truncate',
+          'filename_first',
           -- shorten = { len = 1, exclude = { -2, -1 } },
         },
       },
