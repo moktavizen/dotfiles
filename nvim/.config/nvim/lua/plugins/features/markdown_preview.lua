fba9760e20ecd1265e04ab0456d7b0453ec533ec
Moh Oktavi Azuz Nugraha <moktavizzen@gmail.com>
2024-08-07 18:09:49 +0700

    update markkdown preview nvim installation

--- nvim/.config/nvim/lua/plugins/features/markdown_preview.lua
+++ nvim/.config/nvim/lua/plugins/features/markdown_preview.lua
@@ -1,11 +1,13 @@
 return {
-  'iamcco/markdown-preview.nvim',
-  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  ft = 'markdown',
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && bun install",
   keys = {
     { '<leader>cmp', '<cmd>MarkdownPreviewToggle<cr>', desc = 'Toggle Markodown Preview' },
   },
-  build = function()
-    vim.fn['mkdp#util#install']()
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
   end,
+  ft = { "markdown" },
+
 }
