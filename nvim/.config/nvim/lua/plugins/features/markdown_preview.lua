80fd021b924f06c6f7f8ff64ed1563c7cc9faa3b
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2024-08-11 12:05:32 +0700

    use markdown-preview.nvim binary installation

--- nvim/.config/nvim/lua/plugins/features/markdown_preview.lua
+++ nvim/.config/nvim/lua/plugins/features/markdown_preview.lua
@@ -1,12 +1,12 @@
 return {
+  -- install without yarn or npm
   'iamcco/markdown-preview.nvim',
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  build = 'cd app && npm install',
+  ft = { 'markdown' },
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
   keys = {
     { '<leader>cmp', '<cmd>MarkdownPreviewToggle<cr>', desc = 'Toggle Markodown Preview' },
   },
-  init = function()
-    vim.g.mkdp_filetypes = { 'markdown' }
-  end,
-  ft = { 'markdown' },
 }
