617bd881224f39aaa76ba15a00571bfeba1c69c9
Moh OKtavi Aziz Nugraha <moktavizzen@gmail.com>
2024-10-29 15:24:18 +0700

    Add css formatter

--- nvim/.config/nvim/lua/plugins/lang.lua
+++ nvim/.config/nvim/lua/plugins/lang.lua
@@ -268,6 +268,7 @@ return {
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
         javascript = { 'prettierd' },
+        css = { 'prettierd' },
       },
     },
   },
