84cd357711ca367d8d3b3cdc28bbb2660abfe75c
Moh OKtavi Aziz Nugraha <moktavizzen@gmail.com>
2024-10-29 09:41:15 +0700

    Remove unnecessary opt

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -59,9 +59,8 @@ vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
--- vim.opt.cursorcolumn= true
 vim.opt.cursorlineopt = 'number'
--- vim.opt.cursorlineopt = "both"
+-- vim.opt.cursorcolumn = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
