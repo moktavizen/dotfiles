38c9f6077e97d576808ef682c87ee2f5eff1d4be
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-03-13 13:08:27 +0700

    enable cd to root directory

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -4,12 +4,13 @@ return {
   keys = {
     { '\\', '<cmd>NvimTreeToggle<cr>', desc = 'Toggle NvimTree' },
   },
+  -- See https://github.com/ahmedkhalf/project.nvim#-features
   opts = {
+    sync_root_with_cwd = true,
+    respect_buf_cwd = true,
     update_focused_file = {
       enable = true,
-      update_root = {
-        enable = true,
-      },
+      update_root = true,
     },
   },
 }
