8c8d1dd96590d4e5cecf9be592b6edd31a72cbde
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-03-15 15:38:00 +0700

    simplify lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,32 +3,12 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   -- enabled = false,
   event = 'VeryLazy',
-  config = function()
-    require('lualine').setup {
-      options = {
-        globalstatus = true,
-        component_separators = '',
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = { statusline = { 'snacks_dashboard' } },
-      },
-      sections = {
-        lualine_a = { { 'mode', separator = { left = '' } } },
-        lualine_b = { 'filename' },
-        lualine_c = { 'branch' },
-        lualine_x = { 'searchcount', 'selectioncount' },
-        lualine_y = { 'filetype', 'progress' },
-        lualine_z = {
-          { 'location', separator = { right = '' } },
-        },
-      },
-      inactive_sections = {
-        lualine_a = { 'filename' },
-        lualine_b = { 'branch' },
-        lualine_c = {},
-        lualine_x = { 'searchcount', 'selectioncount' },
-        lualine_y = { 'filetype', 'progress' },
-        lualine_z = { 'location' },
-      },
-    }
-  end,
+  opts = {
+    options = {
+      globalstatus = true,
+      component_separators = '',
+      section_separators = { left = '', right = '' },
+      disabled_filetypes = { statusline = { 'snacks_dashboard' } },
+    },
+  },
 }
