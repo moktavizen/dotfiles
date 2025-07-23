6b5ab4ebd4f6bed00df106fa01885676e2e91feb
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-07-23 18:01:33 +0700

    feat(dev): drop lush theming

--- nvim/.config/nvim/lua/plugins/lush.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'rktjmp/lush.nvim',
-  dependencies = 'rktjmp/shipwright.nvim',
-  enabled = false,
-  -- if you wish to use your own colorscheme:
-  -- { dir = '/absolute/path/to/colorscheme', lazy = true },
-}
--- nvim/.config/nvim/lua/plugins/neutr.lua
+++ nvim/.config/nvim/lua/plugins/neutr.lua
@@ -1,10 +1,9 @@
 return {
-  enabled = true,
   'moktavizen/neutr.nvim',
+  lazy = false,
   priority = 1000,
   init = function()
     vim.cmd.colorscheme 'neutr'
-    -- vim.cmd.colorscheme 'neutr-dev'
   end,
   -- 'folke/tokyonight.nvim',
   -- priority = 1000,
