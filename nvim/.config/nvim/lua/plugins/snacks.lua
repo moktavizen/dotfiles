a569cc2b8ec557bef4ae03595aa96a8fc6a703d4
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-07-24 06:56:07 +0700

    style(dev): format snacks key table

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,22 +3,21 @@ return {
   enabled = true,
   priority = 1000,
   lazy = false,
+  -- stylua: ignore start
   keys = {
-    -- stylua: ignore start
-    { '<leader>sc', function() Snacks.picker.highlights() end, desc = '[S]earch [C]olor highlights' },
-    { '<leader>sh', function() Snacks.picker.help() end, desc = '[S]earch [H]elp' },
-    { '<leader>sk', function() Snacks.picker.keymaps() end, desc = '[S]earch [K]eymaps' },
-    { '<leader>sf', function() Snacks.picker.files() end, desc = '[S]earch [F]iles' },
-    { '<leader>st', function() Snacks.picker.colorschemes() end, desc = '[S]earch [T]heme' },
-    { '<leader>sw', function() Snacks.picker.grep_word() end, desc = '[S]earch current [W]ord' },
-    { '<leader>sg', function() Snacks.picker.grep() end, desc = '[S]earch by [G]rep' },
-    { '<leader>sd', function() Snacks.picker.diagnostics() end, desc = '[S]earch [D]iagnostics' },
-    { '<leader>sr', function() Snacks.picker.resume() end, desc = '[S]earch [R]esume' },
-    { '<leader>s.', function() Snacks.picker.recent() end, desc = '[S]earch Recent Files ("." for repeat)' },
-    { '<leader><leader>', function() Snacks.picker.buffers() end, desc = '[ ] Find existing buffers' },
-    { '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit', },
-    { '<leader>gl', function() Snacks.lazygit.log() end, desc = 'Lazygit Log (cwd)', },
-    -- stylua: ignore end
+    { '<leader>sc',       function() Snacks.picker.highlights() end,   desc = '[S]earch [C]olor highlights'            },
+    { '<leader>sh',       function() Snacks.picker.help() end,         desc = '[S]earch [H]elp'                        },
+    { '<leader>sk',       function() Snacks.picker.keymaps() end,      desc = '[S]earch [K]eymaps'                     },
+    { '<leader>sf',       function() Snacks.picker.files() end,        desc = '[S]earch [F]iles'                       },
+    { '<leader>st',       function() Snacks.picker.colorschemes() end, desc = '[S]earch [T]heme'                       },
+    { '<leader>sw',       function() Snacks.picker.grep_word() end,    desc = '[S]earch current [W]ord'                },
+    { '<leader>sg',       function() Snacks.picker.grep() end,         desc = '[S]earch by [G]rep'                     },
+    { '<leader>sd',       function() Snacks.picker.diagnostics() end,  desc = '[S]earch [D]iagnostics'                 },
+    { '<leader>sr',       function() Snacks.picker.resume() end,       desc = '[S]earch [R]esume'                      },
+    { '<leader>s.',       function() Snacks.picker.recent() end,       desc = '[S]earch Recent Files ("." for repeat)' },
+    { '<leader><leader>', function() Snacks.picker.buffers() end,      desc = '[ ] Find existing buffers'              },
+    { '<leader>gg',       function() Snacks.lazygit() end,             desc = 'Lazygit',                               },
+    { '<leader>gl',       function() Snacks.lazygit.log() end,         desc = 'Lazygit Log (cwd)',                     },
   },
   ---@type snacks.Config
   opts = {
@@ -83,10 +82,11 @@ return {
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠇⠘⢄⠀⠀⠘⠻⠭⠭⠭⠭⠋⠀⠀⡠⠊⢰⣿⠆⠀⠀⠀⠀ ⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣧⢠⡀⠈⢷⣄⠀⠀⠀⠀⠀⠀⠀⣠⠎⠀⠀⠜⣽⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣯⠀⢺⣄⠈⢚⠑⠤⠤⠤⠤⢴⣾⠇⢀⡀⠀⠀⣷⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
+        -- stylua: ignore
         keys = {
-          { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = ' ', key = 'f', desc = 'Find File',    action = ":lua Snacks.dashboard.pick('files')"     },
+          { icon = ' ', key = 'g', desc = 'Find Text',    action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')"  },
         },
       },
     },
