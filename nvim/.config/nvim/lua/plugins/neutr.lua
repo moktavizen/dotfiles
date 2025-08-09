137c8b208d0a3db42e1e507ce1e7b08c4dbc3734
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-08-09 23:49:07 +0700

    chore(dev): match upstream colorscheme spec

--- nvim/.config/nvim/lua/plugins/neutr.lua
+++ nvim/.config/nvim/lua/plugins/neutr.lua
@@ -2,13 +2,7 @@
 return {
   -- 'moktavizen/neutr.nvim',
   dir = '~/Documents/repos/neutr.nvim/',
-  dependencies = { 'echasnovski/mini.colors' },
   priority = 1000,
-  opts = {
-    -- hue = 284,
-    -- bg_chroma = 0.03,
-    -- fg_hue_mix = 100,
-  },
   config = function()
     vim.cmd 'colorscheme neutr'
   end,
