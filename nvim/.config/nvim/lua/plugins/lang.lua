f92ff1212b4e50a9baf24b9ce614e9e9076eafa0
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-08-29 14:02:45 +0700

    feat: decentralized languange config

--- nvim/.config/nvim/lua/plugins/lang.lua
+++ nvim/.config/nvim/lua/plugins/lang.lua
@@ -1,51 +1,116 @@
 -- Plugins that affects language support
-local tools = {
-  lsp = {
-    lua_ls = {
-      settings = {
-        Lua = {
-          completion = {
-            callSnippet = 'Replace',
+return {
+  -- Auto install package for LSPs and related tools
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    dependencies = {
+      -- Listed as dependencies to ensure they load first
+      'mason-org/mason.nvim',
+      'mason-org/mason-lspconfig.nvim',
+    },
+    event = 'VeryLazy',
+    config = function()
+      -- NOTE: Step 1 - Install the LSP/formatter
+      local needed_tools = {
+        'lua_ls',
+        'bashls',
+        'emmet_language_server',
+        'html',
+        'cssls',
+        'ts_ls',
+        'stylua',
+        'shfmt',
+        'prettierd',
+        'biome',
+        'xmlformatter',
+      }
+      require('mason-tool-installer').setup({
+        ensure_installed = needed_tools,
+      })
+
+      -- See https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim/issues/39
+      vim.api.nvim_command('MasonToolsInstall')
+      vim.api.nvim_create_autocmd('User', {
+        desc = 'Start current buffer LSP after installing it',
+        pattern = 'MasonToolsUpdateCompleted',
+        callback = function()
+          vim.schedule(function()
+            vim.api.nvim_command('LspStart')
+          end)
+        end,
+      })
+
+      -- Cleanup unneeded tools
+      local installed_tools = require('mason-registry').get_all_package_names()
+      if #installed_tools > #needed_tools then
+        vim.api.nvim_command('MasonToolsClean')
+      end
+    end,
+  },
+
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    event = 'VeryLazy',
+    config = function()
+      -- NOTE: Step 2 - Configure the LSP if needed
+      vim.lsp.config('lua_ls', {
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = 'Replace',
+            },
           },
         },
-      },
-    },
-    bashls = {},
-    emmet_language_server = {},
-    html = {},
-    cssls = {},
-    ts_ls = {},
-  },
-  formatter = {
-    stylua = {
-      filetypes = { 'lua' },
-    },
-    shfmt = {
-      filetypes = { 'bash', 'sh' },
-      settings = {
-        -- Google's shell style guide: shfmt -i 2 -ci -bn
-        append_args = { '-i', '2', '-ci', '-bn' },
-      },
-    },
-    prettierd = {
-      filetypes = { 'html' },
-    },
-    biome = {
-      -- Sometimes a formatter has several conform-formatters, we can specify the name
-      -- See `:h conform-formatters`
-      name = 'biome-check',
-      filetypes = { 'css', 'javascript', 'typescript', 'json' },
-      settings = {
-        append_args = { '--indent-style=space' },
-      },
-    },
-    xmlformatter = {
-      filetypes = { 'xml' },
-    },
-  },
-}
+      })
 
-return {
+      -- NOTE: Step 3 - Enable the LSP
+      vim.lsp.enable({
+        'lua_ls',
+        'bashls',
+        'emmet_language_server',
+        'html',
+        'cssls',
+        'ts_ls',
+      })
+    end,
+  },
+
+  -- Formatter
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    opts = {
+      -- NOTE: Step 2 - Configure the formatter if needed
+      formatters = {
+        shfmt = {
+          -- Google's shell style guide: shfmt -i 2 -ci -bn
+          append_args = { '-i', '2', '-ci', '-bn' },
+        },
+        -- Sometimes a formatter has several conform-formatters, we can specify the name
+        -- See `:h conform-formatters`
+        ['biome-check'] = {
+          append_args = { '--indent-style=space' },
+        },
+      },
+
+      -- NOTE: Step 3 - Map the formatter
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        bash = { 'shfmt' },
+        sh = { 'shfmt' },
+        html = { 'prettierd' },
+        css = { 'biome-check' },
+        javascript = { 'biome-check' },
+        typescript = { 'biome-check' },
+        json = { 'biome-check' },
+        xml = { 'xmlformatter' },
+      },
+      format_on_save = { lsp_format = 'fallback', timeout_ms = 500 },
+    },
+  },
+
   -- Syntax highlight, edit, and navigate code
   {
     'nvim-treesitter/nvim-treesitter',
@@ -71,61 +136,6 @@ return {
     },
   },
 
-  -- LSP
-  {
-    'neovim/nvim-lspconfig',
-    event = 'VeryLazy',
-    config = function()
-      for lsp_name, lsp_config in pairs(tools.lsp) do
-        vim.lsp.config(lsp_name, lsp_config)
-        vim.lsp.enable(lsp_name)
-      end
-    end,
-  },
-
-  -- Configures Lua LSP for Neovim config,
-  -- used for completion, annotations and signatures of Neovim apis
-  {
-    'folke/lazydev.nvim',
-    ft = 'lua', -- only load on lua files
-    opts = {
-      library = {
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
-
-  -- Formatter
-  {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    opts = function()
-      -- Map formatter tools to conform.nvim
-      local formatters = {}
-      local formatters_by_ft = {}
-      for name, config in pairs(tools.formatter) do
-        local formatter_name = config.name or name
-
-        if config.settings then
-          formatters[formatter_name] = config.settings
-        end
-
-        for _, ft in ipairs(config.filetypes or {}) do
-          formatters_by_ft[ft] = formatters_by_ft[ft] or {}
-          table.insert(formatters_by_ft[ft], formatter_name)
-        end
-      end
-      return {
-        format_on_save = function()
-          return { timeout_ms = 500, lsp_format = 'fallback' }
-        end,
-        formatters = formatters,
-        formatters_by_ft = formatters_by_ft,
-      }
-    end,
-  },
-
   -- Package manager for LSPs and related tools
   {
     'mason-org/mason.nvim',
@@ -140,7 +150,6 @@ return {
   -- Translate LSP name to package name
   {
     'mason-org/mason-lspconfig.nvim',
-    -- Listed as dependencies to ensure they load first
     dependencies = {
       'mason-org/mason.nvim',
       'neovim/nvim-lspconfig',
@@ -152,37 +161,15 @@ return {
     },
   },
 
-  -- Auto install package for LSPs and related tools
+  -- Configures Lua LSP for Neovim config,
+  -- used for completion, annotations and signatures of Neovim apis
   {
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    dependencies = {
-      'mason-org/mason.nvim',
-      'mason-org/mason-lspconfig.nvim',
+    'folke/lazydev.nvim',
+    ft = 'lua', -- only load on lua files
+    opts = {
+      library = {
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
     },
-    event = 'VeryLazy',
-    config = function()
-      local needed_tools = vim.tbl_keys(vim.tbl_extend('force', tools.lsp, tools.formatter))
-      require('mason-tool-installer').setup({
-        ensure_installed = needed_tools,
-      })
-
-      -- See https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim/issues/39
-      vim.api.nvim_command('MasonToolsInstall')
-      vim.api.nvim_create_autocmd('User', {
-        desc = 'Start current buffer LSP after installing it',
-        pattern = 'MasonToolsUpdateCompleted',
-        callback = function()
-          vim.schedule(function()
-            vim.api.nvim_command('LspStart')
-          end)
-        end,
-      })
-
-      -- Cleanup unneeded tools
-      local installed_tools = require('mason-registry').get_all_package_names()
-      if #installed_tools > #needed_tools then
-        vim.api.nvim_command('MasonToolsClean')
-      end
-    end,
   },
 }
