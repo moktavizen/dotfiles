27e99eb676b33bb138e492662145ac1b0183f4cd
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2025-12-22 15:48:02 +0700

    nvim: disable lsp progress

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -5,6 +5,9 @@ return {
   event = 'VeryLazy',
   opts = {
     lsp = {
+      progress = {
+        enabled = false,
+      },
       -- Override markdown rendering so that **cmp** and other plugins use **Treesitter**
       override = {
         ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
