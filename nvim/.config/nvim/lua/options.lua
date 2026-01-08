d4d7d9245c0ca64733e24d70c1eee1d54d6e34a3
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2026-01-08 09:16:12 +0700

    nvim: organize options

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -7,6 +7,30 @@ end)
 vim.o.undofile = true
 vim.o.swapfile = false
 
+vim.o.wrap = false
+vim.o.linebreak = true
+vim.o.breakindent = true
+
+vim.o.scrolloff = 10
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.inccommand = 'split'
+vim.o.updatetime = 250
+
+vim.o.spell = true
+vim.o.spelllang = 'en_us'
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
+
+vim.o.splitright = true
+vim.o.splitbelow = true
+
 vim.o.number = true
 vim.o.numberwidth = 1
 vim.o.statuscolumn = '%l %s%C'
@@ -19,30 +43,6 @@ vim.o.showmode = false
 vim.o.cursorline = true
 vim.o.cursorlineopt = 'number'
 -- vim.o.cursorcolumn = true
-
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = true
-vim.o.smartindent = true
-
-vim.o.wrap = false
-vim.o.linebreak = true
-vim.o.breakindent = true
-
-vim.o.scrolloff = 10
-
-vim.o.splitright = true
-vim.o.splitbelow = true
-
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
-vim.o.inccommand = 'split'
-vim.o.updatetime = 250
-
-vim.o.spell = true
-vim.o.spelllang = 'en_us'
 
 vim.diagnostic.config({
   underline = true,
