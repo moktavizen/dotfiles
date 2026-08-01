ed0a8b40336ad8a2e62ed3707171db1575119105
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2026-08-01 13:34:15 +0700

    Setup silent boot

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.timeout = 0;
+  boot.kernelParams = [ "quiet" ];
+  boot.blacklistedKernelModules = [ "iTCO_wdt" ];
 
   networking.hostName = "thicc430";
 
