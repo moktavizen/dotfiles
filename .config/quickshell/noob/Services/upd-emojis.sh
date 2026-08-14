6c0d884829543d35a671e8e48dd2a6d55ab40520
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2026-08-14 10:09:09 +0700

    Minify emojis json to improve perf

--- .config/quickshell/noob/Services/upd-emojis.sh
+++ .config/quickshell/noob/Services/upd-emojis.sh
@@ -6,8 +6,8 @@
 rm emojis.json
 
 curl -fsSL 'https://github.com/muan/emojilib/raw/main/dist/emoji-en-US.json' \
-  | jq 'to_entries | map({
+  | jq -c 'to_entries | map({
     e: .key,
     n: (.value[0] | gsub("_"; " ")),
     k: (.value | join(" "))
-  })' > emojis.json
+  })' >emojis.json
