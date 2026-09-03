56e318119842d7dea15965a29f04c01f3d61a6fd
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2026-09-03 14:21:31 +0700

    Use dns setting from NixOS wiki

--- configuration.nix
+++ configuration.nix
@@ -39,16 +39,23 @@
   networking.hostName = "thicc430";
 
   networking.networkmanager.enable = true;
-  services.resolved = {
-    enable = true;
-    settings.Resolve = {
-      DNSOverTLS = "true";
-    };
-  };
+  # From https://wiki.nixos.org/wiki/Systemd/resolved#Configuration_Example:_Enforce_secure_DNS
   networking.nameservers = [
     "1.1.1.1"
     "1.0.0.1"
   ];
+  services.resolved = {
+    enable = true;
+    settings.Resolve = {
+      DNSSEC = "true";
+      Domains = [ "~." ];
+      DNSOverTLS = "true";
+      FallbackDNS = [
+        "1.1.1.1"
+        "1.0.0.1"
+      ];
+    };
+  };
 
   time.timeZone = "Asia/Jakarta";
 
